182137e7a8ff04578f18c2155063d2ce4563ae17
Conor Barnes <conorbarnes93@gmail.com>
2022-07-10 15:36:59 -0300

    error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	export function load({ error, status }: { error: any; status: any }) {
+		return {
+			props: {
+				title: `${status}: ${error.message}`
+			}
+		};
+	}
+</script>
+
+<script>
+	export let title = "";
+</script>
+
+<svelte:head>
+	<title>404 / Conor Barnes</title>
+</svelte:head>
+
+<div class="h-[60vh] flex flex-col items-center justify-center">
+	<div class="p-4 w-screen text-white">
+		<p class="text-center">{title}</p>
+	</div>
+</div>
