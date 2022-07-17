c41ca6a1898dd6f62ff77589fc22bda6626d26ed
Conor Barnes <conorbarnes93@gmail.com>
2022-07-17 13:03:12 -0300

    fix languages positioning

--- src/components/Languages.svelte
+++ src/components/Languages.svelte
@@ -18,29 +18,50 @@
 <svelte:window bind:innerWidth={size} />
 {#if size}
 	<div
-		class="flex items-center justify-between  "
+		class="flex items-center justify-center  "
 		style={"width: " + (size / (mobileRatio / 3) + 20)}
 	>
-		<Html size={size / mobileRatio} />
-		<Css size={size / mobileRatio} />
-		<Js size={size / mobileRatio} />
+		<div class="mx-1 lg:mx-0">
+			<Html size={size / mobileRatio} />
+		</div>
+		<div class="mx-1 lg:mx-0">
+			<Css size={size / mobileRatio} />
+		</div>
+		<div class="mx-1 lg:mx-0">
+			<Js size={size / mobileRatio} />
+		</div>
 	</div>
 	<div
-		class="flex flex-wrap items-center justify-between  my-2"
+		class="flex flex-wrap items-center justify-center  my-2"
 		style={"width: " + (size / (mobileRatio / 3) + 20)}
 	>
-		<React size={size / mobileRatio} />
-		<Next size={size / mobileRatio} />
-		<Ts size={size / mobileRatio} />
+		<div class="mx-1 lg:mx-0">
+			<React size={size / mobileRatio} />
+		</div>
+		<div class="mx-1 lg:mx-0">
+			<Next size={size / mobileRatio} />
+		</div>
+
+		<div class="mx-1 lg:mx-0">
+			<Ts size={size / mobileRatio} />
+		</div>
 	</div>
 	<div
-		class="flex flex-wrap items-center justify-between  "
+		class="flex flex-wrap items-center justify-center  "
 		style={"width: " + (size / (mobileRatio / 3) + 20)}
 	>
-		<Node size={size / mobileRatio} />
-		<Go size={size / mobileRatio} />
-		<Rust size={size / mobileRatio} />
+		<div class="mx-1 lg:mx-0">
+			<Node size={size / mobileRatio} />
+		</div>
+
+		<div class="mx-1 lg:mx-0">
+			<Go size={size / mobileRatio} />
+		</div>
+
+		<div class="mx-1 lg:mx-0">
+			<Rust size={size / mobileRatio} />
+		</div>
 	</div>
 {:else}
-	<div class="flex items-center justify-between w-full  " />
+	<div class="flex items-center justify-between w-full" />
 {/if}
