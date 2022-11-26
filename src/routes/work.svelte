f0333e96d63625a611d5abdddad5ea70c8bfc9bf
Conor Barnes <conorbarnes93@gmail.com>
2022-11-25 22:11:53 -0400

    80k

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -23,6 +23,15 @@
 	>
 	<Project
 		id={1}
+		color="#F4F6F7"
+		dark={$themeStore.theme === "dark"}
+		img="images/squares/80k.png"
+		title="80,000 Hours"
+		text="I work at 80,000 Hours as a software engineer on the job board!"
+		link="https://jobs.80000hours.org/"
+	/>
+	<Project
+		id={2}
 		color={$themeStore.theme === "dark" ? "#014421" : "#5BC26A"}
 		dark={$themeStore.theme === "dark"}
 		img="images/squares/tree-sample.png"
@@ -31,7 +40,7 @@
 		link="https://possibleworldstree.com/"
 	/>
 	<Project
-		id={1}
+		id={3}
 		color={$themeStore.theme === "dark" ? "#57534e" : "#1A2D52"}
 		dark
 		img="images/squares/melangepic.png"
@@ -40,7 +49,7 @@
 		link="https://www.melange.io/"
 	/>
 	<Project
-		id={2}
+		id={4}
 		color={$themeStore.theme === "dark" ? "#1e3a8a" : "#6cb6e0"}
 		img="images/squares/starrynightglitchedsquare.png"
 		title="Pixel Sorter"
@@ -48,7 +57,7 @@
 		link="https://pixel-sorter.com/"
 	/>
 	<Project
-		id={3}
+		id={5}
 		color={$themeStore.theme === "dark" ? "#bd93f9" : "#F5CBF3"}
 		img="images/squares/tdpic.png"
 		title="Tonal Distancing"
